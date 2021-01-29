9fa035cca06fbe4d1e6ea31b7247d9ba7606ac62
Julia Vaz <juliavaz.dev@gmail.com>
2021-01-29 11:25:42 -0300

    add projects

---
+++ cSharp/SomaSimples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, soma;
+            Console.WriteLine("Para sair do loop digite o número 0");
+
+            while (true)
+            {
+                Console.Write("Insira o primeiro valor: ");
+                a = float.Parse(Console.ReadLine());
+
+                if (a == 0)
+                {
+                    Console.WriteLine("Saindo do loop");
+                    break;
+                }
+
+                Console.Write("Insira o segundo valor: ");
+                b = float.Parse(Console.ReadLine());
+
+                if (b == 0)
+                {
+                    Console.WriteLine("Saindo do loop");
+                    break;
+                }
+
+                soma = a + b;
+
+                Console.WriteLine("SOMA = " + soma);
+            }
+        }
+    }
+}
