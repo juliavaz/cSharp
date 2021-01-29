d33f642f5ccfcea0c69fb97a555ac8257a51398f
Julia Vaz <juliavaz.dev@gmail.com>
2021-01-29 12:58:18 -0300

    add exercicio de multiplicação

---
+++ cSharp/Exercicio02/Exercicio02.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    class Exercicio02
+    {
+        static void Main(string[] args)
+        {
+            int valor, n, result;
+
+            Console.WriteLine("Insira o valor da tabuada que quer saber!! ");
+            valor = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                result = valor * i;
+                Console.WriteLine(valor + " X " + i + " = " + result);
+            }
+        }
+    }
+}
