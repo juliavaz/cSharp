9af8aae30e1b940ba3134722611adc65f53feed4
Julia Vaz <juliavaz.dev@gmail.com>
2021-02-01 12:35:10 -0300

    alterando a estrutura do projeto

--- src/HelloWorld/HelloWorld.cs
+++ src/HelloWorld/HelloWorld.cs
@@ -2,11 +2,40 @@
 
 namespace HelloWorld
 {
-    class HelloWorld
+    public class HelloWorld
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Soma
+            Console.WriteLine("Digite primeiro o numero ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite segundo o numero ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Soma somaSimples = new Soma();
+            var resultado = somaSimples.SomarDoisNumeros(a, b);
+            Console.WriteLine($"O resultado é : {resultado}");
+            Console.ReadKey();
+
+            // Formatando Números
+            Console.WriteLine("Digite o valor a ser formatado");
+            double saldo = Convert.ToDouble(Console.ReadLine());
+            FormatNumeros formatNumeros = new FormatNumeros();
+            var resultado1 = formatNumeros.NumeroFormatado(saldo);
+            Console.WriteLine($"{resultado1}");
+
+            // Saída de Dados
+            SaidaDados saidaDados = new SaidaDados();
+            var resultado2 = saidaDados.SaidaDeDados();
+            Console.WriteLine($"{resultado2}");
+            Console.ReadKey();
+
+            // Tabuada
+            Console.WriteLine("Insira o valor da tabuada que quer saber!! ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            Tabuada tabuada = new Tabuada();
+            var resultado3 = tabuada.TabelaTabuada(valor);
+            Console.WriteLine($"{resultado3}");
+
         }
     }
 }
---
+++ src/HelloWorld/Soma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Soma
+    {
+        public double SomarDoisNumeros(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
